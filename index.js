14e28402602bf8ea5ff743e6bbce7493c7439228
cryptomehedi <classicalmehedi@gmail.com>
2022-05-23 18:18:11 +0600

    pagesParts

--- index.js
+++ index.js
@@ -11,6 +11,62 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json())
 
+// mongodb connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.BD_PASS}@cluster0.jyw9q.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try {
+        await client.connect()
+        const partsCollection = client.db('manufacture').collection('parts')
+
+        app.get('/allParts', async (req, res) =>{
+            const query = {}
+            const cursor = partsCollection.find(query)
+            const service = await cursor.toArray()
+            res.send(service)
+        })
+
+        // pagenation 
+
+        app.get('/pagesParts',async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+
+            const query = {};
+            const cursor = partsCollection.find(query);
+            let products;
+            if(page || size){
+
+                products = await cursor.skip(page*size).limit(size).toArray()
+            }else{
+                products = await cursor.toArray()
+            }
+            
+            res.send(products)
+        })
+        
+
+        app.get('/allPartsCount', async (req, res) => {
+            const count = await partsCollection.estimatedDocumentCount()
+            res.send({count})
+        })
+
+        app.get('/inventory/:id', async (req, res) =>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const service = await partsCollection.findOne(query)
+            res.send(service)
+        })
+        
+    }
+    finally{
+        
+    }
+}
+run().catch(console.dir)
+
 
 
 
