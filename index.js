3c6de83c0df9a84a048b811687f6d3b48f1a755b
cryptomehedi <classicalmehedi@gmail.com>
2022-05-23 18:29:52 +0600

    db pass fixed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 // mongodb connection
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.BD_PASS}@cluster0.jyw9q.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jyw9q.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
